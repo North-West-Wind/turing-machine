38a6ad075789426ca75af60be4cdeb76d250d892
North-West-Wind <wsyn148@gmail.com>
2025-02-22 11:24:50 +0800

    use react instead

--- frontend/turing-machine-web/src/app.tsx
+++ frontend/turing-machine-web/src/app.tsx
@@ -1,24 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './app.css'
-import { ErrorBoundary, lazy, LocationProvider, Route, Router } from 'preact-iso'
-
-const Landing = lazy(() => import('./pages/landing'));
-const Login = lazy(() => import('./pages/login'));
-const Mode = lazy(() => import('./pages/mode'));
-const Level = lazy(() => import('./pages/level'));
-const Designer = lazy(() => import('./pages/designer'));
+import LandingPage from './pages/landing';
+import LoginPage from './pages/login';
+import ModePage from './pages/mode';
+import LevelPage from './pages/level';
+import DesignerPage from './pages/designer';
+import TutorialPage from './pages/tutorial';
 
 export function App() {
-  return <LocationProvider>
-    <ErrorBoundary>
-      <Router>
-        <Route path='/' component={Landing} />
-        <Route path='/login' component={Login} />
-        <Route path='/mode' component={Mode} />
-        { /* "/level" for level selection. "/level/:id" for level detail. */ }
-        <Route path='/level' component={Level} />
-        <Route path='/level/:id' component={Level} /> 
-        <Route path='/designer' component={Designer} />
-      </Router>
-    </ErrorBoundary>
-  </LocationProvider>;
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/mode' element={<ModePage />} />
+      { /* "/level" for level selection. "/level/:id" for level detail. */ }
+      <Route path='/level' element={<LevelPage />} />
+      <Route path='/level/:id' element={<LevelPage />} /> 
+      <Route path='/designer' element={<DesignerPage />} />
+      <Route path='/tutorial' element={<TutorialPage />} />
+    </Routes>
+  </BrowserRouter>;
 }
