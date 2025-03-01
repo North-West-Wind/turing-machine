5c6c2c38fe27f2036a607001059282a59abdd727
North-West-Wind <wsyn148@gmail.com>
2025-03-01 15:55:07 +0800

    console styling

--- frontend/turing-machine-web/src/components/designer/console.tsx
+++ frontend/turing-machine-web/src/components/designer/console.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import useWindowSize from "../../hooks/useWindowSize";
 import DesignerResizer from "./resizer";
 
@@ -12,7 +12,30 @@ export default function DesignerConsole(props: { onHeightChange: (factor: number
 	useEffect(() => {
 		props.onHeightChange(factor);
 	}, [factor]);
+
+	const [lines, setLines] = useState<{ isInput: boolean, str: string }[]>([]);
+	const [lastLine, setLastLine] = useState("");
+
+	const onChange = (ev: ChangeEvent<HTMLTextAreaElement>) => {
+		const splits = ev.currentTarget.value.split("\n");
+		const lastLineStr = splits.pop()!.slice(2);
+		if (splits.length - lines.length >= 1) {
+			// enter is pressed
+			setLines(lines.concat({ isInput: true, str: splits.pop()!.slice(2) }));
+			setLastLine("");
+		} else {
+			// other key presses
+			setLastLine(lastLineStr);
+		}
+	};
+	
 	return <div className="designer-bottom" style={{ height: y * factor }}>
 		<DesignerResizer onChangeProportion={change => setFactor(initFactor - change)} onSettle={change => setInitFactor(initFactor - change)} />
+		<textarea
+			className="designer-console"
+			value={lines.concat({ isInput: true, str: lastLine }).map(line => `${line.isInput ? "<" : ">"} ${line.str}`).join("\n")}
+			onChange={onChange}
+			spellCheck={false}
+		/>
 	</div>;
 }
