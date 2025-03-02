b4f171a86b7987d07acf292ab479668e89dfc201
24080002-Ye Sing Sen <evelopecu@gmail.com>
2025-03-02 15:28:09 +0800

    Login, mode framework from Vincent

--- frontend/turing-machine-web/src/pages/login.tsx
+++ frontend/turing-machine-web/src/pages/login.tsx
@@ -1,6 +1,36 @@
-import { useLocation } from "react-router-dom";
+import Login_box from '../../components/box'
+import Button_box from '../../components/login_button';
+import {useState} from 'react';
+import '../app.css'
 
+// Correct, yes. Else no
 export default function LoginPage() {
-	const location = useLocation();
-	return <div>TODO: login ({location.pathname})</div>;
+	const [User, saveUser] = useState<string>('');
+	const [Pw, savePw] = useState<string>('');
+
+	function UserChange(value:string){
+		saveUser(value);
+	}
+
+	function PwChange(value: string){
+		savePw(value);
+	}
+
+	return (
+		<div>
+			<h2>Username</h2>
+			<Login_box BoxChange={UserChange} />
+			
+			<br></br>
+
+			<h2>Password</h2>
+			<Login_box BoxChange={PwChange}/>
+			
+
+			<p className="login">
+				<Button_box user={User} password={Pw} ></Button_box>
+			</p>
+
+		</div>
+	);
 }
--- frontend/turing-machine-web/src/pages/mode.tsx
+++ frontend/turing-machine-web/src/pages/mode.tsx
@@ -1,3 +1,15 @@
+import {useNavigate} from "react-router-dom";
+
+
 export default function ModePage() {
-	return <div>TODO: mode</div>;
+	//return <div>TODO: mode</div>;
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<button onClick={function(){navigate("/designer")}}> Sand box</button>
+			<button onClick={function(){navigate("/level")}}>Level</button>	
+		</>
+	);
+
 }
