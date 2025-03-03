24759b8cf3591489558f515c4fb95f7fc71bc12a
24080002-Ye Sing Sen <evelopecu@gmail.com>
2025-03-03 19:44:08 +0800

    New implementation with register function in login

--- frontend/turing-machine-web/src/pages/login.tsx
+++ frontend/turing-machine-web/src/pages/login.tsx
@@ -1,35 +1,44 @@
-import Login_box from '../../components/box'
-import Button_box from '../../components/login_button';
+import Login_box from '../../components/LoginBox'
+import LoginArrangement from '../../components/LoginArrange';
 import {useState} from 'react';
 import '../app.css'
 
-// Correct, yes. Else no
 export default function LoginPage() {
+	// This one is driven by button
+	const [isToggled, setIsToggled] = useState<boolean>(false);
+	let registerNback = isToggled ? "Back" : "Register";
+
+	// Three const are interrupt driven variable by input box
 	const [User, saveUser] = useState<string>('');
 	const [Pw, savePw] = useState<string>('');
-
-	function UserChange(value:string){
-		saveUser(value);
-	}
-
-	function PwChange(value: string){
-		savePw(value);
-	}
+	const [Key, saveKey] = useState<string>('');
+	// Updater functions for the three once triggered interrupt
+	function UserChange(value:string){saveUser(value);}
+	function PwChange(value: string){savePw(value);}
+	function KeyChange(value: string){saveKey(value);}
 
 	return (
 		<div>
 			<h2>Username</h2>
 			<Login_box BoxChange={UserChange} />
-			
-			<br></br>
+			<br/>
 
 			<h2>Password</h2>
 			<Login_box BoxChange={PwChange}/>
+			<br/>
 			
+			{
+				isToggled ?
+				<div>
+					<h2>License Key</h2>
+					<Login_box BoxChange={KeyChange}/>
+				</div>
+				:
+				<></> 
+			}
 
-			<p className="login">
-				<Button_box user={User} password={Pw} ></Button_box>
-			</p>
+			<LoginArrangement isRegistering={isToggled} user={User} password={Pw} licenseKey={Key}/>
+			<button onClick={()=>setIsToggled(!isToggled)}>{registerNback}</button>
 
 		</div>
 	);
