fd805ed596019b4948e6d6e830856e4326a34875
24080002-Ye Sing Sen <evelopecu@gmail.com>
2025-03-03 20:10:35 +0800

    Add the `register implementation and with standardised code and place style

--- frontend/turing-machine-web/src/pages/login.tsx
+++ frontend/turing-machine-web/src/pages/login.tsx
@@ -1,5 +1,5 @@
-import Login_box from '../../components/LoginBox'
-import LoginArrangement from '../../components/LoginArrange';
+import Login_box from '../components/login/login-box'
+import LoginArrangement from '../components/login/login-arrange';
 import {useState} from 'react';
 import '../app.css'
 
@@ -7,7 +7,7 @@ export default function LoginPage() {
 	// This one is driven by button
 	const [isToggled, setIsToggled] = useState<boolean>(false);
 	let registerNback = isToggled ? "Back" : "Register";
-
+	
 	// Three const are interrupt driven variable by input box
 	const [User, saveUser] = useState<string>('');
 	const [Pw, savePw] = useState<string>('');
@@ -16,13 +16,13 @@ export default function LoginPage() {
 	function UserChange(value:string){saveUser(value);}
 	function PwChange(value: string){savePw(value);}
 	function KeyChange(value: string){saveKey(value);}
-
+	
 	return (
 		<div>
 			<h2>Username</h2>
 			<Login_box BoxChange={UserChange} />
 			<br/>
-
+	
 			<h2>Password</h2>
 			<Login_box BoxChange={PwChange}/>
 			<br/>
@@ -36,10 +36,10 @@ export default function LoginPage() {
 				:
 				<></> 
 			}
-
+	
 			<LoginArrangement isRegistering={isToggled} user={User} password={Pw} licenseKey={Key}/>
 			<button onClick={()=>setIsToggled(!isToggled)}>{registerNback}</button>
-
+	
 		</div>
 	);
 }
