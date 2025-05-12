1314d8c652830cd3fe0142d9eda56e7f3801fcd0
LegendsOfSky <67500141+LegendsOfSky@users.noreply.github.com>
2025-05-13 05:12:21 +0800

    backend: tapesinfo

--- backend/TuringMachine.Backend/TuringMachine.Backend.Server/Database/Entity/MachineStorage/TapeInfo.cs
+++ backend/TuringMachine.Backend/TuringMachine.Backend.Server/Database/Entity/MachineStorage/TapeInfo.cs
@@ -1,11 +1,13 @@
-﻿namespace TuringMachine.Backend.Server.Database.Entity.MachineStorage
+﻿using TuringMachine.Backend.Server.Models.Machines.Tapes;
+
+namespace TuringMachine.Backend.Server.Database.Entity.MachineStorage
 {
     internal class TapeInfo
     {
-        public Guid   DesignID      { get; set; }
-        public short  TapeIndex     { get; set; }
-        public byte   TapeType      { get; set; }
-        public string InitialValues { get; set; }
+        public Guid     DesignID      { get; set; }
+        public short    TapeIndex     { get; set; }
+        public TapeType TapeType      { get; set; }
+        public string   InitialValues { get; set; }
 
 
         #region Relationship
---
+++ backend/TuringMachine.Backend/TuringMachine.Backend.Server/DbInteractions/DbMachineInteraction/DbTapeInteraction.cs
@@ -0,0 +1,58 @@
+﻿using TuringMachine.Backend.Server.Database;
+using TuringMachine.Backend.Server.ServerResponses;
+using static TuringMachine.Backend.Server.Models.Misc.ResponseStatus;
+
+#region Type Alias
+using DbTape = TuringMachine.Backend.Server.Database.Entity.MachineStorage.TapeInfo;
+
+using ResponseTape = TuringMachine.Backend.Server.Models.Machines.Tapes.Tape;
+#endregion
+
+namespace TuringMachine.Backend.Server.DbInteractions.DbMachineInteraction
+{
+    internal class DbTapeInteraction
+    {
+        public static ServerResponse<IList<ResponseTape?>> GetTapes(string designID , DataContext db)
+        {
+            IQueryable<DbTape> dbTapes       = db.TapeInfos.Where(tape => tape.DesignID == Guid.Parse(designID));
+            ResponseTape?[]    responseTapes = new ResponseTape?[dbTapes.Count()];
+
+            foreach (DbTape dbTape in dbTapes)
+            {
+                responseTapes[dbTape.TapeIndex] = new ResponseTape
+                {
+                    Type          = dbTape.TapeType ,
+                    InitialValues = dbTape.InitialValues ,
+                };
+            }
+            return new ServerResponse<IList<ResponseTape?>>(SUCCESS , responseTapes);
+        }
+
+        
+        public static ServerResponse InsertTapes(string designID , IList<ResponseTape> tape , DataContext db)
+        {
+            for (short i = 0; i < tape.Count; i++)
+            {
+                db.TapeInfos.Add(
+                    new DbTape
+                    {
+                        DesignID      = Guid.Parse(designID) ,
+                        TapeIndex     = i ,
+                        TapeType      = tape[i].Type ,
+                        InitialValues = tape[i].InitialValues ,
+                    }
+                );
+            }
+            return new ServerResponse(SUCCESS);
+        }
+
+
+        public static ServerResponse DeleteTapes(string designID , DataContext db)
+        {
+            db.TapeInfos.RemoveRange(
+                db.TapeInfos.Where(tape => tape.DesignID == Guid.Parse(designID))
+            );
+            return new ServerResponse(SUCCESS);
+        }
+    }
+}
