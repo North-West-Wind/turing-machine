c8f9b7a704a5c09201503438d60b1ac3c91fef9a
LegendsOfSky <67500141+LegendsOfSky@users.noreply.github.com>
2025-05-13 02:50:31 +0800

    backend: rework on validate token

--- backend/TuringMachine.Backend/TuringMachine.Backend.Server/DbInteractions/UserInteractions/DbAccessTokenInteraction.cs
+++ backend/TuringMachine.Backend/TuringMachine.Backend.Server/DbInteractions/UserInteractions/DbAccessTokenInteraction.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using TuringMachine.Backend.Server.Database;
 using TuringMachine.Backend.Server.Database.Entity.UserManagement;
@@ -14,18 +15,54 @@ namespace TuringMachine.Backend.Server.DbInteractions.UserInteractions
 {
     internal static class DbAccessTokenInteraction
     {
+        /// <returns>
+        ///     Return an access token for a particular user when "SUCCESS". <br/><br/>
+        ///     Status is either "SUCCESS", "USER_NOT_FOUND" or "DUPLICATED_USER".
+        /// </returns>
         public static ServerResponse ValidateToken(string accessToken , DataContext db)
         {
-            using var user = db.Users.Where(user => user.AccessToken == accessToken).GetEnumerator();
+            using IEnumerator<DbUser> user = db.Users.Where(user => user.AccessToken == accessToken).GetEnumerator();
             
-            if (!user.MoveNext()) return ServerResponse.StartTracing<string>(nameof(user) , NO_SUCH_ITEM);
-            DbUser userDb = user.Current;
-            if (user.MoveNext()) return ServerResponse.StartTracing<string>(nameof(user) , DUPLICATED_ITEM);
-            
-            DateTime localDate = DateTime.Now;
-            return localDate > userDb.AccessTokenExpireTime
-                ? ServerResponse.StartTracing<string>(nameof(user) , TOKEN_EXPIRED) 
-                : new ServerResponse(SUCCESS);
+            if (!user.MoveNext()) return ServerResponse.StartTracing<string>(nameof(ValidateToken) , USER_NOT_FOUND);
+            DbUser dbUser = user.Current;
+            if (user.MoveNext()) return ServerResponse.StartTracing<string>(nameof(ValidateToken) , DUPLICATED_USER);
+
+            switch (dbUser.AccessToken , dbUser.AccessTokenExpireTime)
+            {
+                case (null , null):
+                    throw new UnreachableException();
+
+                case (not null , null):
+                    return ServerResponse.StartTracing(nameof(ValidateToken) , BACKEND_ERROR);
+
+                case (not null , not null) when dbUser.AccessTokenExpireTime >= DateTime.Now:
+                    dbUser.AccessTokenExpireTime = DateTime.Now.AddMinutes(30);
+                    return new ServerResponse(SUCCESS);
+
+                case (not null , not null) when dbUser.AccessTokenExpireTime < DateTime.Now:
+#if DEBUG
+                    return new ServerResponse(SUCCESS);
+#else
+                    return new ServerResponse(TOKEN_EXPIRED);
+#endif
+
+                default: throw new UnreachableException();
+            }
+        }
+
+        /// <returns>
+        ///     Return an access token for a particular user when "SUCCESS". <br/><br/>
+        ///     Status is either "SUCCESS", "USER_NOT_FOUND" or "DUPLICATED_USER".
+        /// </returns>
+        /// <remarks> This method will make changes to database once completed. </remarks>
+        public static async Task<ServerResponse> ValidateAndUpdateTokenAsync(string accessToken , DataContext db)
+        {
+            ServerResponse validateTokenResponse = ValidateToken(accessToken , db);
+            if (validateTokenResponse.Status is not SUCCESS)
+                return validateTokenResponse.WithThisTraceInfo(nameof(ValidateAndUpdateTokenAsync) , validateTokenResponse.Status);
+
+            await db.SaveChangesAsync();
+            return validateTokenResponse;
         }
 
 
